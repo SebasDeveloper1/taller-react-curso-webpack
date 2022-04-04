bd6df5bb16fc1afa4799b3a319c375ed63d37bb0
Sebastian-Pedroza <sebaspedroza.developer@gmail.com>
2022-04-03 20:31:47 -0500

    v2.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
